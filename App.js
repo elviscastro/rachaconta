e3d37c97b6f08241c03955a05ac93bdd0f6b9473
lcaf1 <85023752+lcaf1@users.noreply.github.com>
2022-10-07 10:24:32 -0300

    Update App.js

--- App.js
+++ App.js
@@ -23,6 +23,7 @@ export default function App() {
         <Stack.Screen name="MesasNovo" component={MesasNovo} />
         <Stack.Screen name="MesaDetalhes" component={MesaDetalhes} />
         <Stack.Screen name="Celso" component={MesaDetalhes} />
+        <Stack.Screen name="Zezinho" component={MesaDetalhes} />
         <Stack.Screen name="CriarUsuario" component={CriarUsuario} />
         <Stack.Screen name="Consumo" component={Consumo} />
         <Stack.Screen name="Main" component={Main} />
